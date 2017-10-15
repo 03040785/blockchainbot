a544a8efd08f010cd0390f143757a0be4fa4e1c4
GuangXian He <guangxianHE@GuangXian.local>
2017-10-15 21:26:57 +0200

    new cacul

--- simulator.py
+++ simulator.py
@@ -2,15 +2,22 @@
 import json
 import urllib.request
 import time
+import numpy as np
 
+# for macbook python3
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
 walletBitcoin = 0.0
 walletDollar = 100000.0
-firstrun = True
 transection = 0.002
+oldPrice = []
+buyPrice = []
+sellPrice = []
+coeffBuy = 0.5
+coeffSell = 0.5
+timeLoop = 1
 
 print ('Starting simulator...')
 def getMidPrice():
@@ -23,35 +30,61 @@ def getSellPrice():
         html = response.read()
     return float(json.loads(html)['last_price'])
 
-oldPrice = currentPrice = initPrice = getSellPrice()
-
+currentPrice = getSellPrice()
+midPrice = getMidPrice()
 
 def getTransectionfee():
     return transection * currentPrice
 
+buyPrice.append(currentPrice + getTransectionfee())
+sellPrice.append(currentPrice - getTransectionfee())
+currentSell =  currentPrice - getTransectionfee()
+currentBuy =  currentPrice + getTransectionfee()
+arverageBuy = sum(buyPrice)/len(buyPrice)
+arverageSell = sum(sellPrice)/len(sellPrice)
 
 def getTotalNetWorth():
     return walletDollar + (walletBitcoin * currentPrice)
 
 def showTotalNetWorth():
+    print ('MidPrice:', midPrice)
     print ('Total bitcoin:', walletBitcoin, 'walletDollar', walletDollar)
     print ('Total networth:', getTotalNetWorth(), 'profit', getTotalNetWorth() - totalNetWorthStart )
+    print ("Arverage buy price:", sum(buyPrice)/len(buyPrice))
+    print ("Arverage sell price:", sum(sellPrice)/len(sellPrice))
 
 totalNetWorthStart = getTotalNetWorth()
 
 while True:
+    timeLoop = 1 + timeLoop
     currentPrice = getSellPrice()
-    print ('old;',oldPrice,'currentPrice;',currentPrice)
-    if currentPrice > oldPrice and walletBitcoin > 0:
-        print ("sell")
-        walletBitcoin = walletBitcoin - 1
-        walletDollar = walletDollar + currentPrice - getTransectionfee()
-    elif currentPrice < oldPrice and walletDollar >0:
-        print ("buy")
-        walletBitcoin = walletBitcoin + 1
-        walletDollar = walletDollar - currentPrice - getTransectionfee()
-    showTotalNetWorth()
+    midPrice = getMidPrice()
+    oldPrice.append(currentPrice)
+    previousPrice = np.mean(oldPrice)
+    currentBuy  =  currentPrice + getTransectionfee()
+    currentSell =  currentPrice - getTransectionfee()
+    arverageBuy = sum(buyPrice)/len(buyPrice)
+    arverageSell = sum(sellPrice)/len(sellPrice)
+    profit = getTotalNetWorth() - totalNetWorthStart
+    print ('---------timeLoop: ',timeLoop)
+    print ('previousPrice: ',previousPrice,'currentPrice: ',currentPrice)
+    print ('currentBuy: ',currentBuy,'currentSell: ',currentSell)
 
-    oldPrice = currentPrice
+
+    if  profit > 0 and currentSell > previousPrice and walletBitcoin > 0:
+        print ("sell....")
+        sellPrice.append(currentPrice - getTransectionfee())
+        print ("Arverage sell price:", arverageSell)
+
+        walletBitcoin = walletBitcoin - coeffSell
+        walletDollar = walletDollar + coeffSell * currentSell
+    elif currentBuy < previousPrice  * 1.001 and walletDollar > currentBuy:
+        print ("buy......")
+        buyPrice.append(currentPrice + getTransectionfee())
+        print ("Arverage buy price:", arverageBuy)
+
+        walletBitcoin = walletBitcoin + coeffBuy
+        walletDollar = walletDollar - coeffBuy * currentBuy
+    showTotalNetWorth()
 
     time.sleep(3)
